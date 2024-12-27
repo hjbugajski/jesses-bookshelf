78fef5f84925912d8135c050fa5cdc5ec4f5560e
Henry Bugajski <henry.bug.dev@gmail.com>
2024-12-27 11:33:46 -0600

    chore: create migrations

--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241110_183932 from './20241110_183932';
+import * as migration_20241227_173326 from './20241227_173326';
 
 export const migrations = [
   {
     up: migration_20241110_183932.up,
     down: migration_20241110_183932.down,
     name: '20241110_183932',
+  },
+  {
+    up: migration_20241227_173326.up,
+    down: migration_20241227_173326.down,
+    name: '20241227_173326'
   },
 ];
