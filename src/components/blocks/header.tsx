adb7bf3f0d8a99f422d6d264ba2eaf39073de4d7
Henry Bugajski <henry.bug.dev@gmail.com>
2025-05-25 10:05:39 -0400

    chore: upgrade to tailwindcss v4

--- src/components/blocks/header.tsx
+++ src/components/blocks/header.tsx
@@ -13,9 +13,9 @@ export const HeaderBlock = ({ className, block: { heading, image }, ...props }: 
     {typeof image === 'string' ? null : (
       <PayloadImage
         {...image}
-        className="h-48 w-48 rounded border-3 border-violet-400 shadow-lg shadow-violet-300/50"
+        className="h-48 w-48 rounded-4xl border-3 border-violet-400 shadow-lg shadow-violet-300/50"
       />
     )}
-    <h1 className="text-3xl font-bold xs:text-4xl">{heading}</h1>
+    <h1 className="xs:text-4xl text-3xl font-bold">{heading}</h1>
   </header>
 );
