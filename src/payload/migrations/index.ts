c0c51644c06379cca8ca9c52d81b3c270dcee9de
Henry Bugajski <henry.bug.dev@gmail.com>
2025-11-16 16:39:24 -0500

    chore: update dependencies (#30)

--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20241110_183932 from './20241110_183932';
 import * as migration_20241227_173326 from './20241227_173326';
 import * as migration_20250830_181756 from './20250830_181756';
 import * as migration_20251101_180520 from './20251101_180520';
+import * as migration_20251116_213518 from './20251116_213518';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20251101_180520.up,
     down: migration_20251101_180520.down,
-    name: '20251101_180520'
+    name: '20251101_180520',
+  },
+  {
+    up: migration_20251116_213518.up,
+    down: migration_20251116_213518.down,
+    name: '20251116_213518'
   },
 ];
--- src/payload/payload-types.ts
+++ src/payload/payload-types.ts
@@ -262,10 +262,6 @@ export interface PayloadLockedDocument {
     | ({
         relationTo: 'users';
         value: string | PayloadUsersCollection;
-      } | null)
-    | ({
-        relationTo: 'payload-kv';
-        value: string | PayloadKv;
       } | null);
   globalSlug?: string | null;
   user: {
